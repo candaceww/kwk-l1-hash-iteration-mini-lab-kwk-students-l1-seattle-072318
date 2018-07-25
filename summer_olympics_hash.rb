ea13492355762ef1a1f2b672ece3a7a1b8c6bbcf
candaceww <candacew224@gmail.com>
2018-07-25 21:14:46 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,19 +1,19 @@
-def create_olympics_hash
+# def create_olympics_hash
 
-  places_array= ["Sydney", "Athens", "Beijing", "London"]
-  years_array= [2000, 2004, 2008, 2012]
-  create_olympics_hash = {}
-  places_years=0
-  places_array.each do |places|
-    create_olympics_hash[places]=years_array[places_years]
-    places_years+=1
-end
-create_olympics_hash.each do |locations,date|
+#   places_array= ["Sydney", "Athens", "Beijing", "London"]
+#   years_array= [2000, 2004, 2008, 2012]
+#   create_olympics_hash = {}
+#   places_years=0
+#   places_array.each do |places|
+#     create_olympics_hash[places]=years_array[places_years]
+#     places_years+=1
+# end
+# create_olympics_hash.each do |locations,date|
 
-end 
-return create_olympics_hash
-end 
-puts create_olympics_hash
+# end 
+# return create_olympics_hash
+# end 
+# puts create_olympics_hash
 # def add_a_key_value_pair
 #   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 # end
